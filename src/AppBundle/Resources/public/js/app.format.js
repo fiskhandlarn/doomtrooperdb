77d49bcf15d4adcffc85542211e22370ec2dacff
ograr <oskar.joelson@goteborgsregionen.se>
2019-08-15 22:18:59 +0200

    Remove trailing dots for card info in js

--- src/AppBundle/Resources/public/js/app.format.js
+++ src/AppBundle/Resources/public/js/app.format.js
@@ -1,55 +1,59 @@
 (function app_format(format, $)
-{
+ {
 
-    /**
-     * @memberOf format
-     */
-    format.name = function name(card)
-    {
-        return card.name;
-    }
+   /**
+    * @memberOf format
+    */
+   format.name = function name(card)
+   {
+     return card.name;
+   }
 
-    format.faction = function faction(card)
-    {
-        return '<span class="fg-' + card.faction_code + ' icon-' + card.faction_code + '"></span> ' + card.faction_name + '. ';
-    }
+   format.faction = function faction(card)
+   {
+     return '<span class="fg-' + card.faction_code + ' icon-' + card.faction_code + '"></span> ' + card.faction_name + '. ';
+   }
 
-    /**
-     * @memberOf format
-     */
-    format.expansion = function expansion(card)
-    {
-        var text = card.expansion_name + '. ';
-        return text;
-    }
+   /**
+    * @memberOf format
+    */
+   format.expansion = function expansion(card)
+   {
+     var text = card.expansion_name;
+     return text;
+   }
 
-    /**
-     * @memberOf format
-     */
-    format.info = function info(card)
-    {
-        var text = '<span class="card-type">' + card.type_name + '. </span>';
-        console.log('TODO warriors and warzones');
-        switch(card.type_code) {
-            case 'warrior':
-                text += Translator.trans('card.info.shoot') + ': ' + card.shoot + '. ';
-                break;
-        }
-        return text;
-    };
+   /**
+    * @memberOf format
+    */
+   format.info = function info(card)
+   {
+     var text = '<span class="card-type">' + card.type_name + '</span>';
+     switch(card.type_code) {
+         case 'warrior':
+         case 'warzone':
+           text += ' ';
+           text += Translator.trans('card.info.fight') + ': ' + card.fight + '. ';
+           text += Translator.trans('card.info.shoot') + ': ' + card.shoot + '. ';
+           text += Translator.trans('card.info.armor') + ': ' + card.armor + '. ';
+           text += Translator.trans('card.info.value') + ': ' + card.value + '. ';
+           break;
+     }
+     return text;
+   };
 
-    /**
-     * @memberOf format
-     */
-    format.text = function text(card)
-    {
-        var text = card.text || '';
-        text = text.replace(/\[(\w+)\]/g, '<span class="icon-$1"></span>')
-        text = text.split("\n").join('</p><p>');
-        if(card.designer) {
-            text = text + '<p class="card-designer">' + card.designer + '</p>';
-        }
-        return '<p>' + text + '</p>';
-    };
+   /**
+    * @memberOf format
+    */
+   format.text = function text(card)
+   {
+     var text = card.text || '';
+     text = text.replace(/\[(\w+)\]/g, '<span class="icon-$1"></span>')
+     text = text.split("\n").join('</p><p>');
+     if(card.designer) {
+       text = text + '<p class="card-designer">' + card.designer + '</p>';
+     }
+     return '<p>' + text + '</p>';
+   };
 
-})(app.format = {}, jQuery);
+ })(app.format = {}, jQuery);
